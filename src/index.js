ad5e76ca04c8e9053cc38b1a24755a397de73e81
Dinh Long <trandoandinhlong@gmail.com>
2024-11-21 12:29:05 +0700

    config

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './Login';
-import Signup from './Signup'
+import Login from '~/pages/Login/Login';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Signup />
     <Login />
-    
   </React.StrictMode>
 );
 
