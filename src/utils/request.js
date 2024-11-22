801e53887ba5b8f5ef93a071412c52eee651eae5
Dinh Long <trandoandinhlong@gmail.com>
2024-11-22 12:50:33 +0700

    sửa file request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: 'https://tiktok.fullstack.edu.vn/api/',
+    baseURL: '',
 });
 
 export const get = async (path, option = {}) => {
